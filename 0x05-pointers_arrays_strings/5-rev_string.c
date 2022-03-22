e37b9e77c1377a8c5adbb0352de5145ce8531269
goodness <gesystemz1@gmail.com>
2022-03-21 18:43:54 -0700

    trials

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,21 +8,18 @@
 
 void rev_string(char *s)
 {
-	int i, max, half;
-	char first, last;
+	int len, i, half;
+	char temp;
 
+	for (len = 0; s[len] != '\0'; len++)
+	;
 	i = 0;
-	while (s[i] != '\0')
+	half = len / 2;
+
+	while (half--)
 	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
 		i++;
 	}
-	max = i - 1;
-	half = max / 2;
-	while (half >= 0)
-	{
-		first = s[max - half];
-		last = s[half];
-		s[max - half] = last;
-		half--;
-	}
-}
