8ee59fdcc6d213a3e22d6ad6f65e19890a29d392
goodness <gesystemz1@gmail.com>
2022-03-24 18:21:25 -0700

    mutiples

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,7 @@
  * @n: amount of bytes used from src.
  * Return: the pointer to dest.
  */
-char *-strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int count = 0, count2 = 0;
 
